171e1bb044dc24820a819aa7e8baa7fd89d0b8f2
陳人傑 <h220641963@gmail.com>
2017-10-27 17:31:20 +0800

    選擇性路由參數 參數可有可無時

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,13 @@ Route::get('student/{student_no}/score', function ($student_no) {
     return "學號:".$student_no."的所有成績";
 });
 
-
+//參數有多個
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號:".$student_no."的".$subject."成績";
+});
+
+//參數可有可無時
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject =null) {
+    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
 
