385a3378d19e2be9d9b21eee6fb9a6a5f056eab3
陳人傑 <h220641963@gmail.com>
2017-10-27 17:49:44 +0800

    //正規表達式限制參數 使用param方法來替較為常用的參數進行統一的格式定義

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,12 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject =null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 })->where(['student_no'=>'s[0-9]{10}','subject'=>' chiness | english | math)']);
+
+//正規表達式限制參數 使用param方法來替較為常用的參數進行統一的格式定義
+Route::pattern('student_no','s[0-9]{10}');
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
+});
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject =null) {
+    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+})->where(['subject'=>'chinese | english | math']);
