269fd0fb237d3285bddd0b655b51c65518f82a2b
陳人傑 <h220641963@gmail.com>
2017-10-27 17:40:50 +0800

    正規表達式限制參數 將參數作一些格式限制

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ Route::get('student/{student_no}', function ($student_no) {
 Route::get('student/{student_no}/score', function ($student_no) {
     return "學號:".$student_no."的所有成績";
 });
-
 //參數有多個
+
 Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號:".$student_no."的".$subject."成績";
 });
@@ -19,3 +19,10 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
 
+//正規表達式限制參數 將參數作一些格式限制
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
+})->where(['student_no'=>'s[0-9]{10}']);
+Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject =null) {
+    return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+})->where(['student_no'=>'s[0-9]{10}','subject'=>' chiness | english | math)']);
