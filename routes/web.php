ac245636d9cb73d806e7ca67073a798668266dce
陳人傑 <h220641963@gmail.com>
2017-10-27 17:23:10 +0800

    基礎路由參數 參數只有一個

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+//參數只有一個
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
 });
+Route::get('student/{student_no}/score', function ($student_no) {
+    return "學號:".$student_no."的所有成績";
+});
+
