86bd061d48a3121700e758b506ed18b5408d6b70
s3A432087 陳人傑 <h220641963@gmail.com>
2017-11-24 17:22:12 +0800

    錯誤更正: nav視圖上的排行榜路由錯誤。

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,9 +1,11 @@
+
+
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="http://localhost:8000/">HelloLaravel</a>
+        <a class="navbar-brand" href="/">HelloLaravel</a>
         <ul class="nav navbar-nav">
             <li>
-                <a href="#">排行榜</a>
+                <a href="/board">排行榜</a>
             </li>
         </ul>
     </div>
